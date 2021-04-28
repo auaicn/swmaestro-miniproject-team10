7a82935618e322293394d025df8c51dca5ea0e91
Seungwoo Jung <seungwoojung@Seungwooui-Macmini.local>
2021-04-28 19:19:38 +0900

    Cron JOB 알람발송 기능

--- cron/index.js
+++ cron/index.js
@@ -1,87 +1,87 @@
 const cron = require('node-cron');
 const Schedule = require('../models/schedule');
+const libKakaoWork = require('../libs/kakaoWork');
 
 exports.cronAlarmJob = () => {
-  cron.schedule('30 * * * * *', ()=> {
-      const now = new Date();
+  // 매 00분 30초마다 실행
+  cron.schedule('30 * * * * *', async ()=> {
+
+    const startTime = new Date();
+    startTime.setHours(startTime.getHours() + 9);
+
+    Schedule.find({
+      date:{$gte:new Date(Date.now() + (3600000*9) - 1000 * 60 * 2),
+        $lt:new Date(Date.now() + (3600000*9) + 1000 * 60 * 2)} //한국시간 앞뒤 2분
+      },function(err,schedules){
+      if(err) {
+        console.log(`cron error: '${err}'`);
+      }
       
-      now.setHours(now.getHours() + 9);
-  
-      Date.prototype.yyyymmddhhmm = function(glue) {
-        if(glue == null) {
-          glue = "-";
-        }
-        let yyyy = this.getFullYear();
-        let mm = this.getMonth() < 9 ? "0" + (this.getMonth() + 1) : (this.getMonth() + 1); // getMonth() is zero-based
-        let dd  = this.getDate() < 10 ? "0" + this.getDate() : this.getDate();
-        let hh = this.getHours() < 10 ? "0" + this.getHours() : this.getHours();
-        let min = this.getMinutes() < 10 ? "0" + this.getMinutes() : this.getMinutes();
-        return "".concat(yyyy).concat(glue).concat(mm).concat(glue).concat(dd).concat("T").concat(hh).concat(":").concat(min);
-      };
-  
-      console.log(now.yyyymmddhhmm());
-      
-      const table = {
-          conversation_id : "1153118",
-          date : '2021-04-27T23:18:00',
-          content : "확인용",
-          link: "https://www.naver.com"
-      };
-      
-      Schedule.find({ date:{$gte:new Date(Date.now() + (3600000*9) - 1000 * 60 * 60),
-          $lt:new Date(Date.now() + (3600000*9) + 1000 * 60 * 60)} //한국시간 앞뒤 1시간
-        },function(err,schedules){
-        if(err) return res.status(500).json({error:err});
-        if(!schedules) return res.status(404).json({error: 'schedules not found'});
-        res.json(schedules);
-      })
-      /*
-      이 구간에서 db에서 table을 가져오거나 하면 될 것 같습니다.
-      
-      */
-      
-      console.log(table.date.substring(0,16));
-       
-      if(table.date.substring(0, 16)===now.yyyymmddhhmm().substring(0,16)){
-          
-      libKakaoWork.sendMessage({
-          conversationId: table.conversation_id,
-            "text": "Push alarm message",
-            "blocks": [
-              {
-                    "type": "header",
-                    "text": "알림이 있습니다!",
-                    "style": "blue"
-              },
-              {
-                    "type": "description",
-                    "term": "일시",
-                    "content": {
-                      "type": "text",
-                      "text": table.date,
-                      "markdown": false
-                  },
-                    "accent": true
-              },
-              {
+      schedules.map(schedule => sendMessage(schedule, startTime))
+    })
+  })
+}
+
+const sendMessage = (schedule, startTime) => {
+  scheduleTime = dateToString(schedule.date);
+  startTime = dateToString(startTime);
+
+  if(scheduleTime.substring(0, 16) === startTime.substring(0,16)){
+    libKakaoWork.sendMessage({
+        conversationId: schedule.conversation_id,
+          "text": "Push alarm message",
+          "blocks": [
+            {
+                  "type": "header",
+                  "text": "알림이 있습니다!",
+                  "style": "blue"
+            },
+            {
+                  "type": "description",
+                  "term": "일시",
+                  "content": {
+                    "type": "text",
+                    "text": dateToDisplayString(schedule.date),
+                    "markdown": false
+                },
+                  "accent": true
+            },
+            {
+              "type": "text",
+              "text": schedule.content,
+              "markdown": true
+            },
+            {
+              "type": "context",
+              "content": {
                 "type": "text",
-                "text": table.content,
+                "text": schedule.link,
                 "markdown": true
               },
-              {
-                "type": "context",
-                "content": {
-                  "type": "text",
-                  "text": table.link,
-                  "markdown": true
-                },
-                "image": {
-                  "type": "image_link",
-                  "url": "https://t1.kakaocdn.net/kakaowork/resources/block-kit/context/zip@3x.png"
-                }
+              "image": {
+                "type": "image_link",
+                "url": "https://t1.kakaocdn.net/kakaowork/resources/block-kit/context/zip@3x.png"
               }
-            ]
-        })
-      }
-  })
+            }
+          ]
+      })
+    }
+}
+
+const dateToString = (date, glue="-") => {
+  const yyyy = date.getFullYear();
+  const mm = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1); // getMonth() is zero-based
+  const dd  = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  const hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  return "".concat(yyyy).concat(glue).concat(mm).concat(glue).concat(dd).concat("T").concat(hh).concat(":").concat(min);
+}
+
+const dateToDisplayString = (date) => {
+  const yyyy = date.getFullYear();
+  const mm = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1); // getMonth() is zero-based
+  const dd  = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  const hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  return `${yyyy}년 ${mm}월 ${dd}일\n${hh}시 ${min}분`
 }

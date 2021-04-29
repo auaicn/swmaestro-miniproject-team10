0ba845aa34ba67380e8aa353d0e29ad5fa5e629b
Go Gwangseo <rufflain918@gmail.com>
2021-04-29 21:16:17 +0900

    Delete Comment

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ const index = require('./routes/index');
 const app = express();
 
 // CONNECT TO MONGODB SERVER
-// var db = mongoose.connection;
-// db.on('error', console.error);
-// db.once('open', function(){
-//     // CONNECTED TO MONGODB SERVER
-//     console.log("Connected to mongod server");
-// });
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+     // CONNECTED TO MONGODB SERVER
+     console.log("Connected to mongod server");
+});
 
-// mongoose.connect('mongodb://localhost/chat');
+mongoose.connect('mongodb://localhost/chat');
 
 // DEFINE MODEL
-// var schedule = require('./models/schedule');
+var schedule = require('./models/schedule');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
